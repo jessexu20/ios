8c13b0b88c0f7aa9d45f4db33393f4b1647dbdb1
Jesse Xu <jesse.xu.20@gmail.com>
2016-03-05 16:06:22 -0500

    add facebook

---
+++ FoodTracker/Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  FoodTracker
+//
+//  Created by Jesse on 3/2/16.
+//  Copyright © 2016 Jesse. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+#endif /* Bridging_Header_h */
--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MealViewController: UIViewController, UITextFieldDelegate,UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIPickerViewDelegate {
+class MealViewController: UIViewController, UITextFieldDelegate,UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIPickerViewDelegate,FBSDKLoginButtonDelegate {
 
     //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
@@ -44,7 +44,18 @@ class MealViewController: UIViewController, UITextFieldDelegate,UIImagePickerCon
             ratingControl.rating = meal.rating
         }
                 checkValidMealName()
-        
+        if (FBSDKAccessToken.currentAccessToken() != nil)
+        {
+            // User is already logged in, do work such as go to next view controller.
+        }
+        else
+        {
+            let loginView : FBSDKLoginButton = FBSDKLoginButton()
+            self.view.addSubview(loginView)
+            loginView.center = self.view.center
+            loginView.readPermissions = ["public_profile", "email", "user_friends"]
+            loginView.delegate = self
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -65,7 +76,51 @@ class MealViewController: UIViewController, UITextFieldDelegate,UIImagePickerCon
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return mealSelection[component][row]
     }
+    //MARK: Facebook Login Delegate
+    // Facebook Delegate Methods
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        print("User Logged In")
+        
+        if ((error) != nil)
+        {
+            // Process error
+        }
+        else if result.isCancelled {
+            // Handle cancellations
+        }
+        else {
+            // If you ask for multiple permissions at once, you
+            // should check if specific permissions missing
+            if result.grantedPermissions.contains("email")
+            {
+                // Do work
+            }
+        }
+    }
     
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        print("User Logged Out")
+    }
+    func returnUserData()
+    {
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: nil)
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            
+            if ((error) != nil)
+            {
+                // Process error
+                print("Error: \(error)")
+            }
+            else
+            {
+                print("fetched user: \(result)")
+                let userName : NSString = result.valueForKey("name") as! NSString
+                print("User Name is: \(userName)")
+                let userEmail : NSString = result.valueForKey("email") as! NSString
+                print("User Email is: \(userEmail)")
+            }
+        })
+    }
     // MARK: UITextFieldDelegate
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         // Hide the keyboard.
